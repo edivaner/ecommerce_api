c0322497586fbcacf60f463b0b6f6fea18661085
leandro <leandrorosadev@gmail.com>
2024-11-03 15:17:51 -0300

    Incluindo logos

--- database/migrations/2024_10_20_022653_create_products_table.php
+++ database/migrations/2024_10_20_022653_create_products_table.php
@@ -18,12 +18,16 @@ return new class extends Migration
             $table->softDeletes();
             $table->string('description');
             $table->char('ean', 13)->unique();
-            $table->decimal('price', 10, 2);
+            // $table->decimal('price', 10, 2); // VER A POSSIBILIDADE DE DESENVOLVER TABELA DE PRECOS
             // $table->unsignedBigInteger('stock_id');
             // $table->unsignedBigInteger('departament_id');
+            // $table->unsignedBigInteger('price_id');
 
             // $table->foreign('stock_id')->references('id')->on('stocks');
             // $table->foreign('departament_id')->references('id')->on('departaments');
+            // $table->foreign('price_id')->references('id')->on('prices'); 
+            
+            // QUANDO FOR CADASTRAR PRICE NA PRICETYPES, CADASTRAR EM CENTAVOS, NO CASO R$ 1,00 = 100 CENTAVOS E ASSIM O PRICE VAI SER INTEIRO SEMPRE
         });
     }
 
